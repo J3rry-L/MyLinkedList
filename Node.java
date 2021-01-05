22992e9f87c3c627fd44d5734fe500424b97d6a0
J3rry-L <jliang21@stuy.edu>
2021-01-05 13:49:59 -0500

    Implemented get/set methods

--- Node.java
+++ Node.java
@@ -1,5 +1,27 @@
 public class Node{
- private String data;
- private Node next,prev;
- //write get/set methods for all three instance variables.
+  private String data;
+  private Node next,prev;
+  public Node(String data, Node next, Node prev){
+    this.data = data;
+    this.next = next;
+    this.prev = prev;
+  }
+  public String getData(){
+    return data;
+  }
+  public void setData(String newData){
+    data = newData;
+  }
+  public Node getNext(){
+    return next;
+  }
+  public void setNext(Node newNext){
+    next = newNext;
+  }
+  public Node getPrev(){
+    return prev;
+  }
+  public void setPrev(Node newPrev){
+    prev = newPrev;
+  }
 }
