0efbd959e97a9c6c1bdf967e42983e19a8256dd1
J3rry-L <jliang21@stuy.edu>
2021-01-05 15:18:36 -0500

    Tested get method

--- Tester.java
+++ Tester.java
@@ -15,9 +15,15 @@ public class Tester{
     System.out.println(list1);
     list1.add(0, "element0");
     System.out.println(list1);
-    list1.add(-8, "element7");
+    list1.add(-8, "element8");
     System.out.println(list1);
-    list1.add(123, "element7");
+    list1.add(123, "element8");
     System.out.println(list1);
+    list1.add(7, "element7");
+    System.out.println(list1);
+    System.out.println(list1.get(-5));
+    System.out.println(list1.get(0));
+    System.out.println(list1.get(5));
+    System.out.println(list1.get(10));
   }
 }
