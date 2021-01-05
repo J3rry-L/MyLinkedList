1f678be90646a7497a5adebfd28aab3611e8e54d
J3rry-L <jliang21@stuy.edu>
2021-01-05 14:28:06 -0500

    Created test cases to test add and toString methods

--- Tester.java
+++ Tester.java
@@ -1,5 +1,13 @@
 public class Tester{
   public static void main(String[] args){
-    
+    MyLinkedList list1 = new MyLinkedList();
+    list1.add("element0");
+    System.out.println(list1);
+    list1.add("element1");
+    System.out.println(list1);
+    list1.add("element2");
+    System.out.println(list1);
+    list1.add("element3");
+    System.out.println(list1);
   }
 }
