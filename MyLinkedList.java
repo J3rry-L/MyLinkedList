36739edde60d1a4eb0a6776a0fafa67e10557f6d
J3rry-L <jliang21@stuy.edu>
2021-01-05 14:27:20 -0500

    Created first add method and toString method and did prelim testing

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,13 +1,52 @@
 public class MyLinkedList{
- private int size;
- private Node start,end;
-
-
- public int size();
- public boolean add(String value);
- public boolean add(int index, String value);
- public String get(int index);
- public String set(int index, String value);
- public String toString();
+  private int size;
+  private Node start,end;
+  public MyLinkedList(){
+    size = 0;
+    start = null;
+    end = null;
+  }
+  public int size(){
+    return size;
+  }
+  public boolean add(String value){
+    if (size == 0){
+      Node node = new Node(value, null, null);
+      start = node;
+      end = node;
+      size++;
+      return true;
+    }
+    else{
+      Node node = new Node(value, null, end);
+      end.setNext(node);
+      end = node;
+      size++;
+      return true;
+    }
+  }
+  public boolean add(int index, String value){
+    return false;
+  }
+  public String get(int index){
+    return null;
+  }
+  public String set(int index, String value){
+    return null;
+  }
+  public String toString(){
+    String result = "";
+    Node current = start;
+    result += "[";
+    while (current != null){
+      if (current != start){
+        result += ", ";
+      }
+      result += current.getData();
+      current = current.getNext();
+    }
+    result += "]";
+    return result;
+  }
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
